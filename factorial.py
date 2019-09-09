3d12bfb1bacf26ebee7ff49580d3e918a7bcf94b
PatZarama <patzarama@gmail.com>
2019-09-08 23:31:56 -0500

    ejercicio factorial

---
+++ factorial.py
@@ -0,0 +1,18 @@
+'''
+Exercise of making a factorial number since "n" number
+Developer: Diego Felipe Zarama Jojoa
+'''
+
+#library
+import os
+
+#factorial
+
+x = int(raw_input("write a number: ")
+a = 1
+b = 2
+
+while b >= x:
+    a *= b
+    b += 1
+    print("the factorial of", x,"! =",a)
